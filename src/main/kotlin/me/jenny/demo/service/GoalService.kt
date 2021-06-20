a8d25054ae52ae8f5bca45f2e4d5484b3153f7cb
kyh1126 <kyh901126@nate.com>
2021-06-20 20:12:54 +0900

    main: 서비스 레이어 테스트코드에 맞게 재작성

--- src/main/kotlin/me/jenny/demo/service/GoalService.kt
+++ src/main/kotlin/me/jenny/demo/service/GoalService.kt
@@ -1,7 +1,6 @@
 package me.jenny.demo.service
 
 import me.jenny.demo.domain.goal.GoalHistory
-import me.jenny.demo.domain.goal.GoalHistoryRepository
 import me.jenny.demo.domain.goal.GoalRepository
 import me.jenny.demo.service.dto.GoalRequest
 import org.springframework.stereotype.Service
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class GoalService(
     private val goalRepository: GoalRepository,
-    private val goalHistoryRepository: GoalHistoryRepository
 ) {
     @Transactional
     fun createGoal(goalRequest: GoalRequest) {
--- src/test/kotlin/me/jenny/demo/service/GoalServiceTest.kt
+++ src/test/kotlin/me/jenny/demo/service/GoalServiceTest.kt
@@ -3,7 +3,6 @@ package me.jenny.demo.service
 import me.jenny.demo.domain.DayOfWeekChoice.*
 import me.jenny.demo.domain.Progress
 import me.jenny.demo.domain.goal.Goal
-import me.jenny.demo.domain.goal.GoalHistory
 import me.jenny.demo.domain.goal.GoalHistoryRepository
 import me.jenny.demo.domain.goal.GoalRepository
 import me.jenny.demo.service.dto.GoalRequest
@@ -30,6 +29,21 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDate
 import java.util.*
 
+/**
+ * Service layer test
+ *
+ * #1. 서비스 레이어는 데이터의 CRUD 를 Repository 에 위임하고 트랜잭션을 관리하는 것이 주요 책임이다.
+ * - JPA 를 사용한다면 DTO <-> Entity 간 변환도 서비스 레이어에서 처리하는 것을 선호한다. 이유는 이 변환을 컨트롤러 레이어에서 수행한다면 Lazy 조회 시 LazyInitializationException 이 발생할 위험에 노출되기 때문이다.
+ * - 물론 이 경우에도 실제 DTO <-> Entity 간 변환 로직 자체는 DTO 에 담고, 서비스 레이어에서는 DTO 의 변환 로직을 호출할 뿐이다.
+ *
+ * #2. Repository 의 동작은 위의 Repository 테스트에서 이미 확인했으므로, 서비스 레이어에서는 Repository 메서드가 제대로 호출되는지만 확인하면 되므로 실제 Repository 대신 Mock Repository 를 사용하면 된다. 따라서 Mockito 필요
+ * - 필요하다면 @SpringBootTest(classes = {A.class, B.class, ...})도 사용 가능하다.
+ *
+ * #3. Mock 을 사용하므로 실제 저장/조회가 발생하지 않는다.
+ * - 생성, 삭제, 조회 시에는 생성/삭제/조회를 호출하는 메서드 호출 여부를 verify 하면 된다.
+ * - 수정 시에는 명시적으로 save()나 saveAndFlush()가 호출되지 않을 수도 있으므로 verify 로는 테스트가 불가능하므로 Mock 에 사용되는 엔티티나 DTO 의 상태 변경을 assert 한다.
+ * - Mock 에 사용되는 엔티티나 DTO 를 생성하는 부분이 매우 번거로울 수 있다.
+ */
 @Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(classes = [GoalService::class])
@@ -39,9 +53,6 @@ internal class GoalServiceTest(private val goalService: GoalService) {
     @MockBean
     lateinit var goalRespository: GoalRepository
 
-    @MockBean
-    lateinit var goalHistoryRepository: GoalHistoryRepository
-
     private val weekdays = setOf(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY)
 
     @Test
@@ -50,7 +61,7 @@ internal class GoalServiceTest(private val goalService: GoalService) {
         // given
         val goalRequest = GoalRequest(
             title = "TDD 프로젝트 완성",
-            startDt = LocalDate.of(2021, 6, 6),
+            startDt = LocalDate.now().minusDays(1),
             totalGoal = 100,
             unitGoal = 5,
             attendDates = weekdays
@@ -70,7 +81,6 @@ internal class GoalServiceTest(private val goalService: GoalService) {
         actualGoal!!
 
         verify(goalRespository, times(1)).save(mockGoal)
-        verify(goalHistoryRepository, times(1)).save(any(GoalHistory::class.java))
         assertEquals(mockGoal, actualGoal)
         assertEquals(actualGoal.status, Progress.IN_PROGRESS)
         assertEquals(actualGoal.attendDates, weekdays)
